90c0beae2884b69a35aed3b9f55a67ce6ca08db6
Nicholas Stevens <nichstev@gmail.com>
2025-12-02 07:45:31 +0100

    Need to fix a small logic error in counting my passes. I dare say almost there?

--- day1/day1.exs
+++ day1/day1.exs
@@ -10,13 +10,16 @@ defmodule FindPassword do
   defp hit_zero([], _pos, count), do: count
 
   defp hit_zero([h | t], pos, count) do 
+    full = abs(div(h, 100))
     new_pos = Integer.mod(h + pos, 100)
-    case new_pos do
-      0 -> hit_zero(t, new_pos, count + 1)
-      _ -> hit_zero(t, new_pos, count)
-    end 
+    crossed = crossed(h, new_pos, pos)
+    hit_zero(t, new_pos, count + full + crossed)
   end
 
+  defp crossed(h, new_pos, pos) when h > 0 and new_pos < pos, do: 1
+  defp crossed(h, new_pos, pos) when h < 0 and new_pos > pos, do: 1
+  defp crossed(_h, _new_pos, _pos), do: 0
+  
   defp convert(<<"L", num::binary>>), do: String.to_integer(num) * -1
   defp convert(<<"R", num::binary>>), do: String.to_integer(num)
 end
