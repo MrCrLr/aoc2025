cca4f39f9070c488fb1311632580673809eadef0
Nicholas Stevens <nichstev@gmail.com>
2025-12-02 07:06:34 +0100

    Part one day one done. Glad I re-read the puzzle description.

--- day1/day1.exs
+++ day1/day1.exs
@@ -4,31 +4,19 @@ defmodule FindPassword do
     |> File.stream!()
     |> Enum.map(&String.trim/1)
     |> Enum.map(&convert/1)
-    |> pass_zero(50, 0)
+    |> hit_zero(50, 0)
   end
 
-  defp pass_zero([], _pos, count), do: count
+  defp hit_zero([], _pos, count), do: count
 
-  defp pass_zero([h | t], pos, count) when h >= 100 do
-    pass_zero([h - 100 | t], pos, count + 1)
-  end
-  
-  defp pass_zero([h | t], pos, count) when h <= -100 do
-    pass_zero([h + 100 | t], pos, count + 1)
-  end
-
-  defp pass_zero([h | t], pos, count) when h + pos >= 100 do
-    curr = Integer.mod(h + pos, 100)
-    pass_zero(t, curr, count + 1) 
+  defp hit_zero([h | t], pos, count) do 
+    new_pos = Integer.mod(h + pos, 100)
+    case new_pos do
+      0 -> hit_zero(t, new_pos, count + 1)
+      _ -> hit_zero(t, new_pos, count)
+    end 
   end
 
-  defp pass_zero([h | t], pos, count) when h + pos < 0 do
-    curr = Integer.mod(h + pos, 100)
-    pass_zero(t, curr, count + 1) 
-  end
-
-  defp pass_zero([h | t], pos, count), do: pass_zero(t, h + pos, count) 
-  
   defp convert(<<"L", num::binary>>), do: String.to_integer(num) * -1
   defp convert(<<"R", num::binary>>), do: String.to_integer(num)
 end
