eb119f1634471e9920e183ea2ab7249a214c957c
Nicholas Stevens <nichstev@gmail.com>
2025-12-03 19:24:04 +0100

    Solution for day 2 part 2

--- day2/day2.exs
+++ day2/day2.exs
@@ -14,19 +14,34 @@ defmodule FindInvalid do
     new_acc = 
       for num <- h1..h2, reduce: acc do
         acc ->
-          str = Integer.to_string(num)
-          len = String.length(str)
-          acc + splitter(str, len, num)
+          digits = num |> Integer.to_string() |> String.graphemes() 
+          len = Enum.count(digits)
+          acc + chunker(digits, len, num)
       end
     process(t, new_acc)
-  end  
-   
-  defp splitter(str, len, num) when rem(len, 2) == 0 do
-    {left, right} = String.split_at(str, div(len, 2))
-    halves_equal?(left, right, num)
-  end
-  defp splitter(_str, _len, _num), do: 0
-  defp halves_equal?(left, right, num) when left == right, do: num 
-  defp halves_equal?(_left, _right, _num), do: 0
+  end 
+ 
+  defp chunker(_digits, len, _num) when len < 2, do: 0   
 
+  defp chunker(digits, len, num) do
+    for x <- 1..(div(len, 2) + 1), reduce: 0 do
+      0 -> 
+        _chunker(digits, len, x, num)
+      acc ->
+        acc
+    end
+  end
+
+  defp _chunker(digits, len, x, num) 
+       when rem(len, x) == 0 and div(len, x) >= 2 do
+    digits 
+    |> Enum.chunk_every(x) 
+    |> Enum.dedup()
+    |> is_repeat?(num)
+  end
+
+  defp _chunker(_digit, _len, _x, _num), do: 0
+
+  defp is_repeat?([_only], num), do: num
+  defp is_repeat?(_, _num), do: 0
 end
