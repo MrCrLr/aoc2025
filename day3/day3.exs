648c7831642bc28943aae4c4898029b17b6c3ba5
Nicholas Stevens <nichstev@gmail.com>
2025-12-04 06:15:24 +0100

    Make code more readable

--- day3/day3.exs
+++ day3/day3.exs
@@ -7,16 +7,27 @@ defmodule FindJoltage do
   end
   
   defp process([], joltage), do: joltage
+
   defp process([bank | rest], joltage) do
     bank = 
       bank 
       |> String.graphemes() 
       |> Enum.map(&String.to_integer/1)
     
-    battery1 = bank |> Enum.slice(0..-2//1) |> Enum.max()
+    battery1 = 
+       bank 
+       |> Enum.slice(0..-2//1) 
+       |> Enum.max()
+
     index = Enum.find_index(bank, fn x -> x == battery1 end)
-    battery2 = bank |> Enum.slice(index + 1..length(bank)-1) |> Enum.max()
+
+    battery2 = 
+      bank 
+      |> Enum.slice(index + 1..length(bank)-1) 
+      |> Enum.max()
+
     battery = battery1 * 10 + battery2
+
     process(rest, joltage + battery)
   end
 end
