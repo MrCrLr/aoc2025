5ecb94e565768be97c916cc18a3022c23e66e4d7
Nicholas Stevens <nichstev@gmail.com>
2025-12-06 12:49:30 +0100

    Build Map for processing data

--- day5/day5.exs
+++ day5/day5.exs
@@ -1,9 +1,21 @@
 defmodule FreshItems do
   def run(file) do
-    lines =
+    [ range_lines, int_lines ] =
       File.stream!(file)
-      |> Enum.map(&String.trim/1)
+      |> Stream.map(&String.trim/1)
+      |> Enum.split_while(&(&1 != ""))
+    digits = 
+      int_lines
+      Enum.drop_while(&(&1 == ""))
+      Enum.map(&String.to_integer/1)
+    ranges = 
+      range_lines
+      |> Enum.map(fn line ->
+        [a, b] = String.split(line, "-")
+        String.to_integer(a)..String.to_integer(b)
+      end)
 
+    %{ digits: digits, ranges: ranges }
   end
 end
 
