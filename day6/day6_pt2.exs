7d48d05a1aaf6a2b6d8a7412a47348d240823674
Nicholas Stevens <nichstev@gmail.com>
2025-12-09 19:14:18 +0100

    Solve for day 6, part 2

--- day6/day6_pt2.exs
+++ day6/day6_pt2.exs
@@ -7,23 +7,11 @@ defmodule MathHomework2 do
       |> Enum.to_list()
     
     {digits, [operations]} = Enum.split(rows, length(rows) - 1)
-    digits =
-      digits
-      |> Enum.map(&String.graphemes/1)
-      |> Enum.zip()
-      |> Enum.map(&Tuple.to_list/1)
-      |> Enum.map(&List.to_string/1)
-      |> Enum.map(&String.trim/1)      
-      |> str_to_num_list([],[])
 
-    # pivot = build_pivot_map(digits, operations)
+    equations = build_equations(digits, operations)
+    result = evaluate(equations)
 
-    IO.inspect(digits, label: "Digits")
-    IO.inspect(operations, label: "Operations")
-    # IO.inspect(pivot, label: "Pivot")
-
-    # result = evaluate(pivot)
-    # IO.inspect(result, label: "Final Result")
+    IO.inspect(result, label: "Final Result")
   end
 
   defp str_to_num_list([], curr, acc), do: [curr | acc] 
@@ -34,30 +22,33 @@ defmodule MathHomework2 do
     str_to_num_list(t, [String.to_integer(h) | curr], acc)
   end
 
-  defp evaluate(pivot) do
-    pivot
-    |> Enum.map(fn
-      {:+, values} ->
-        values
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.sum()
+  defp build_equations(digits, operations) do
+    digits =
+      digits
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.map(&List.to_string/1)
+      |> Enum.map(&String.trim/1)      
+      |> str_to_num_list([],[])
+    
+    equations = 
+      operations
+      |> String.split()
+      |> Enum.reverse()
+      |> Enum.map(&String.to_atom/1)
+      |> Enum.zip(digits)
 
-      {:*, values} ->
-        values
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.product()
-    end)
-    |> Enum.sum()
+    equations
   end
 
-  defp build_pivot_map(digits, operations) do
-    columns = 
-      digits 
-      |> Enum.zip() 
-      |> Enum.map(&Tuple.to_list/1)
-    
-    operations
-    |> Enum.map(&String.to_atom/1)
-    |> Enum.zip(columns)
+  defp evaluate(equations) do
+    equations
+    |> Enum.map(fn
+      { :+, values } -> Enum.sum(values)
+      { :*, values } -> Enum.product(values)
+    end)
+
+    |> Enum.sum()
   end
 end
