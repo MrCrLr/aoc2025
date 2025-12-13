c04272d68b194be3ccc6e36094d304a5024130ea
Nicholas Stevens <nichstev@gmail.com>
2025-12-13 12:43:35 +0100

    Flawed logic in adding to and counting groups.

--- day8/day8.exs
+++ day8/day8.exs
@@ -18,13 +18,50 @@ defmodule XYZ do
     sorted_distances = 
       clean_and_sort(distance_pairs)
 
-    IO.inspect(sorted_distances)
+    grouped_distances = 
+      create_groups(sorted_distances, 0, 0, [{:g1, []}])
+
+    IO.inspect(grouped_distances)
+
+  end
+
+  defp create_groups(sorted_distances, index, _count, acc) 
+       when index == length(sorted_distances), do: acc
+  defp create_groups(sorted_distances, 0, 0, acc) do
+    {{x, y}, _dist} = Enum.at(sorted_distances, 0)
+    create_groups(sorted_distances, 1, 1, Keyword.put(acc, :g1, [x, y]))
+  end
+  defp create_groups(sorted_distances, index, count, acc) do
+    {{x, y}, _dist} = Enum.at(sorted_distances, index)
+
+    keys = Keyword.keys(acc)
+
+    matching_key = 
+      Enum.find(keys, fn key ->
+        values = Keyword.get(acc, key)
+        x in values or y in values
+      end)
+
+    new_acc =
+      if matching_key do
+        Keyword.update!(acc, matching_key, fn values ->
+          Enum.uniq(values ++ [x, y])
+        end)
+      else
+        new_key = String.to_atom("g#{count + 1}")
+        Keyword.put(acc, new_key, [x, y])
+      end 
+
+    new_count =
+      if matching_key, do: count, else: count + 1
+
+    create_groups(sorted_distances, index + 1, new_count, new_acc)
   end
 
   defp clean_and_sort(distance_pairs) do 
     distance_pairs
     |> Enum.uniq_by(fn {_coords, dist} -> dist end)
-    |> Enum.sort_by( fn {_coords, dist} -> dist end, :asc)
+    |> Enum.sort_by(fn {_coords, dist} -> dist end, :asc)
   end
 
   defp get_distances(_graph, {i, _j}, len, acc) when i == len, do: acc
