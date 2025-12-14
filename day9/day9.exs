74532d89029c9bca41cda54bb36a3c585b06bb39
Nicholas Stevens <nichstev@gmail.com>
2025-12-14 10:48:24 +0100

    Solve Day 9, part 1 with simple brute force max area rectangle... lol

--- day9/day9.exs
+++ day9/day9.exs
@@ -11,53 +11,19 @@ defmodule Rectangle do
        end)
       |> Enum.map(&List.to_tuple/1)
 
-    hull = Hull.convex_hull(points)    
+    get_max_area(points)
 
-    max_area =
-      for {x1, y1} <- hull,
-          {x2, y2} <- hull,
-          x1 != x2 and y1 != y2 do
-        (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
-      end
-      |> Enum.max()
-
-    IO.puts(max_area)
   end
-
-defmodule Hull do
-  def convex_hull(points) when length(points) <= 2, do: points
-
-  def convex_hull(points) do
-    points
-    |> Enum.sort()
-    |> build_hull()
-  end
-
-  defp build_hull(points) do
-    lower = build_half(points, [])
-    upper = build_half(Enum.reverse(points), [])
-    
-    Enum.drop(lower, -1) ++ Enum.drop(upper, -1)
-  end
-
-  defp build_half([], acc), do: acc
-
-  defp build_half([p | rest], acc) do
-    acc = remove_turns(acc, p)
-    build_half(rest, [p | acc])
-  end
-
-  defp remove_turns([p2, p1 | rest], p3) do
-    if cross(p1, p2, p3) <= 0 do
-      remove_turns([p1 | rest], p3)
-    else
-      [p2, p1 | rest]
+  
+  defp get_max_area(points) do
+    for {a, b} <- points, {x, y} <- points,
+        {a, b} < {x, y}, reduce: 0 do 
+    best ->
+        max(best, area(a, b, x, y))
     end
   end
 
-  defp remove_turns(acc, _p), do: acc
-
-  defp cross({x1, y1}, {x2, y2}, {x3, y3}) do
-    (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)
-  end
+  defp area(a, b, x, y) do
+    (abs(a - x) + 1) * (abs(b - y) + 1)
+  end   
 end
